f7a14713425ea2475336bb219009a498caa857d3
Anthony <skydabutterrs+github@gmail.com>
2023-02-25 18:48:21 -0500

    colorScheme update

--- app/js/global.js
+++ app/js/global.js
@@ -1,5 +1,13 @@
 // default dark mode colorScheme
-document.getElementsByTagName('html')[0].classList.add('dark-mode');
+if (localStorage.getItem('colorScheme')) {
+    let savedColorScheme = localStorage.getItem('colorScheme');
+    document.getElementsByTagName('html')[0].classList.add(savedColorScheme);
+    document.getElementById("colorScheme").innerHTML = `<b>${savedColorScheme == 'dark-mode' ?  'Light Mode' : 'Dark Mode'}</b>`
+} else {
+    let savedColorScheme = 'dark-mode';
+    localStorage.setItem('colorScheme', 'dark-mode');
+    document.getElementsByTagName('html')[0].classList.add('dark-mode');
+}
 
 function colorScheme() {
 
@@ -7,13 +15,14 @@ function colorScheme() {
     if (html.classList.contains("dark-mode") == true) {
         html.classList.remove("dark-mode")
         html.classList.add("light-mode")
-        document.getElementById("colorScheme").innerHTML = "Dark Mode"
-
+        document.getElementById("colorScheme").innerHTML = "<b>Dark Mode</b>"
+        localStorage.setItem('colorScheme', 'light-mode');
     }
 
     else {
         html.classList.remove("light-mode")
         html.classList.add("dark-mode")
-        document.getElementById("colorScheme").innerHTML = "Light Mode"
+        document.getElementById("colorScheme").innerHTML = "<b>Light Mode</b>"
+        localStorage.setItem('colorScheme', 'dark-mode');
     }
 }
--- app/js/login.js
+++ app/js/login.js
@@ -6,9 +6,13 @@ defLoginTextareas = ['Enter a valid Email Address...', 'Enter a Username...']
 loginTextareas.forEach((area, ind) => {
     area.onfocus = () => {
         area.innerText = ''
+        area.style.color = 'black';
+        area.style.fontStyle = 'normal';
     }
 
     area.onblur = () => {
         area.innerText = defLoginTextareas[ind]
+        area.style.color = '#8a8a8a';
+        area.style.fontStyle = 'italic';
     }
 });
